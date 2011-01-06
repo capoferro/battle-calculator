9fc75fa9f67d5f18b3a8b70ef3ff1be06c275679
Josiah Kiehl <josiah@bluepojo.com>
2011-01-05 23:18:17 -0500

    suppose clojurers use - instead of _

--- clj/com/bluepojo/skirmisher/probability.clj
+++ clj/com/bluepojo/skirmisher/probability.clj
@@ -4,70 +4,70 @@
   (:require [clojure.contrib.trace :as trace]))
 
 (test/with-test
-  (defn to_wound
+  (defn to-wound
     ([s t]
        (let [x (+ 4 (- t s))]
          (max 2 (min 6 x)))))
-  (test/is (= (to_wound 3 3) 4))
-  (test/is (= (to_wound 3 4) 5))
-  (test/is (= (to_wound 3 2) 3))
-  (test/is (= (to_wound 3 1) 2))
-  (test/is (= (to_wound 4 1) 2))
-  (test/is (= (to_wound 5 1) 2))
-  (test/is (= (to_wound 6 1) 2))
-  (test/is (= (to_wound 7 1) 2))
-  (test/is (= (to_wound 4 6) 6))
-  (test/is (= (to_wound 4 7) 6)))
+  (test/is (= (to-wound 3 3) 4))
+  (test/is (= (to-wound 3 4) 5))
+  (test/is (= (to-wound 3 2) 3))
+  (test/is (= (to-wound 3 1) 2))
+  (test/is (= (to-wound 4 1) 2))
+  (test/is (= (to-wound 5 1) 2))
+  (test/is (= (to-wound 6 1) 2))
+  (test/is (= (to-wound 7 1) 2))
+  (test/is (= (to-wound 4 6) 6))
+  (test/is (= (to-wound 4 7) 6)))
 
 (test/with-test
-  (defn to_hit
-    ([attacker_ws defender_ws]
-       (let [difference (- defender_ws attacker_ws)]
-         (cond (> difference attacker_ws) 5
+  (defn to-hit
+    ([attacker-ws defender-ws]
+       (let [difference (- defender-ws attacker-ws)]
+         (cond (> difference attacker-ws) 5
                (< difference 0)  3
                :else 4))))
-  (test/is (= (to_hit 3 3) 4))
-  (test/is (= (to_hit 3 4) 4))
-  (test/is (= (to_hit 3 5) 4))
-  (test/is (= (to_hit 3 6) 4))
-  (test/is (= (to_hit 3 7) 5))
-  (test/is (= (to_hit 3 8) 5))
-  (test/is (= (to_hit 3 2) 3))
-  (test/is (= (to_hit 3 1) 3)))
+  (test/is (= (to-hit 3 3) 4))
+  (test/is (= (to-hit 3 4) 4))
+  (test/is (= (to-hit 3 5) 4))
+  (test/is (= (to-hit 3 6) 4))
+  (test/is (= (to-hit 3 7) 5))
+  (test/is (= (to-hit 3 8) 5))
+  (test/is (= (to-hit 3 2) 3))
+  (test/is (= (to-hit 3 1) 3)))
 
 (test/with-test
-  (defn to_save_armor
+  (defn to-save-armor
     ([s armor]
        (let [modifier (max 0 (- s 3))]
          (max 2 (+ armor  modifier)))))
-  (test/is (= (to_save_armor 5 2) 4))
-  (test/is (= (to_save_armor 5 3) 5))
-  (test/is (= (to_save_armor 5 5) 7))
-  (test/is (= (to_save_armor 1 3) 3))
-  (test/is (= (to_save_armor 2 3) 3))
-  (test/is (= (to_save_armor 3 3) 3))
-  (test/is (= (to_save_armor 3 1) 2))
-  (test/is (= (to_save_armor 3 -1) 2)))
+  (test/is (= (to-save-armor 5 2) 4))
+  (test/is (= (to-save-armor 5 3) 5))
+  (test/is (= (to-save-armor 5 5) 7))
+  (test/is (= (to-save-armor 1 3) 3))
+  (test/is (= (to-save-armor 2 3) 3))
+  (test/is (= (to-save-armor 3 3) 3))
+  (test/is (= (to-save-armor 3 1) 2))
+  (test/is (= (to-save-armor 3 -1) 2)))
 
 (test/with-test
-  (defn to_save_ward
+  (defn to-save-ward
     ([ward]
        (max 2 ward)))
-  (test/is (= (to_save_ward 0) 2))
-  (test/is (= (to_save_ward 7) 7))
-  (test/is (= (to_save_ward -5) 2))
-  (test/is (= (to_save_ward 3) 3)))
+  (test/is (= (to-save-ward 0) 2))
+  (test/is (= (to-save-ward 7) 7))
+  (test/is (= (to-save-ward -5) 2))
+  (test/is (= (to-save-ward 3) 3)))
 
 (test/with-test
-  (defn probability_of
-    ([target_roll]
-       (/ (- 7 (min 7 (max 1 target_roll)))
+  (defn probability-of
+    ([target-roll]
+       (/ (- 7 (min 7 (max 1 target-roll)))
           6)))
-  (test/is (= (probability_of 6) (/ 1 6)))
-  (test/is (= (probability_of 7) 0))
-  (test/is (= (probability_of 4) (/ 3 6)))
-  (test/is (= (probability_of 0) 1))
-  (test/is (= (probability_of -1) 1))
+  (test/is (= (probability-of 6) (/ 1 6)))
+  (test/is (= (probability-of 7) 0))
+  (test/is (= (probability-of 4) (/ 3 6)))
+  (test/is (= (probability-of 0) 1))
+  (test/is (= (probability-of -1) 1))
   )
 
 (test/with-test
@@ -80,16 +80,16 @@
   (test/is (= (inverted 0) 1)))
 
 (test/with-test
-  (defn to_wound_on_single_attack
+  (defn to-wound-on-single-attack
     ([attacker defender]
-       (reduce * [(probability_of (to_hit (:ws attacker) (:ws defender)))
-                  (probability_of (to_wound (:s attacker) (:t defender)))
-                  (inverted (probability_of (to_save_armor (:s attacker) (:armor defender))))
-                  (inverted (probability_of (to_save_ward  (:ward defender))))])))
+       (reduce * [(probability-of (to-hit (:ws attacker) (:ws defender)))
+                  (probability-of (to-wound (:s attacker) (:t defender)))
+                  (inverted (probability-of (to-save-armor (:s attacker) (:armor defender))))
+                  (inverted (probability-of (to-save-ward  (:ward defender))))])))
   ;; 3 to hit, 4 to wound, no saves: 2/3 hit, 1/2 wound, 0/6 saves
-  (test/is (= (to_wound_on_single_attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5)) (/ 1 3)))
-  (test/is (= (to_wound_on_single_attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :armor 1)) (/ 1 9)))
-  (test/is (= (to_wound_on_single_attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :armor 1 :ward 4)) (/ 1 18)))
-  (test/is (= (to_wound_on_single_attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :ward 4)) (/ 1 6))))
+  (test/is (= (to-wound-on-single-attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5)) (/ 1 3)))
+  (test/is (= (to-wound-on-single-attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :armor 1)) (/ 1 9)))
+  (test/is (= (to-wound-on-single-attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :armor 1 :ward 4)) (/ 1 18)))
+  (test/is (= (to-wound-on-single-attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :ward 4)) (/ 1 6))))
 
 
