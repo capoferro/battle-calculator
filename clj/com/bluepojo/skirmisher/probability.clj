28329093fced508d97159a58c631e0e8ba9b387c
Josiah Kiehl <josiah@bluepojo.com>
2011-01-09 22:40:02 -0500

    to-kill

--- clj/com/bluepojo/skirmisher/probability.clj
+++ clj/com/bluepojo/skirmisher/probability.clj
@@ -93,3 +93,67 @@
   (test/is (= (to-wound-on-single-attack (model/create :ws 5 :s 5) (model/create :ws 3 :t 5 :ward 4)) (/ 1 6))))
 
 
+
+(test/with-test
+  (defn kill-impossible?
+    ([attacks wounds remaining_chance]
+       (or (< attacks wounds)
+           (< attacks 0)
+           (= remaining_chance 0))))
+  (test/is (= (kill-impossible? 3 3 1)   false))
+  (test/is (= (kill-impossible? 3 3 0)   true))
+  (test/is (= (kill-impossible? -1 -1 1) true))
+  (test/is (= (kill-impossible? 2 3 1)   true)))
+
+(test/with-test
+  (defn kill-completed?
+    ([attacks wounds exact]
+       (cond (and (= wounds 0) (= attacks 0) (= exact)) true
+             (and (= wounds 0) (not exact)) true
+             :else false)))
+  (test/is (= (kill-completed? 0 1 true)  false))
+  (test/is (= (kill-completed? 1 1 false) false))
+  (test/is (= (kill-completed? 0 0 true)  true))
+  (test/is (= (kill-completed? 1 0 false) true))
+  (test/is (= (kill-completed? 0 0 false) true)))
+
+(defn- perform-kill
+  ([attacks wounds chance_to_wound remaining_chance exact]
+     (if (kill-impossible? attacks wounds remaining_chance) 0
+         (if (kill-completed? attacks wounds exact) remaining_chance
+             (let [remaining_chance_for_wound_branch (* chance_to_wound remaining_chance)
+                   remaining_chance_for_fail_branch (- remaining_chance remaining_chance_for_wound_branch)
+                   wound_branch (perform-kill (- attacks 1) (- wounds 1) chance_to_wound, remaining_chance_for_wound_branch exact)
+                   fail_branch (perform-kill (- attacks 1) wounds chance_to_wound remaining_chance_for_fail_branch exact)]
+               (+ wound_branch fail_branch))))))
+
+(test/with-test
+  (defn to-kill
+    ([attacker defender]
+       (let [chance_to_wound (to-wound-on-single-attack attacker defender)]
+         (perform-kill (:a attacker) (:w defender) chance_to_wound 1 false))))
+  (let [defender (model/create :ws 3 :t 3 :w 1)]
+    (test/is (= (to-kill (model/create :ws 3 :s 3 :a 1) defender) 1/4))
+    (test/is (= (to-kill (model/create :ws 4 :s 3 :a 1) defender) 1/3))
+    (test/is (= (to-kill (model/create :ws 1 :s 3 :a 1) defender) 1/6))
+    (test/is (= (to-kill (model/create :ws 3 :s 4 :a 1) defender) 1/3))
+    (test/is (= (to-kill (model/create :ws 3 :s 5 :a 1) defender) 5/12)))
+  (let [defender (model/create :ws 3 :t 3 :w 2)]
+    (test/is (= (to-kill (model/create :ws 3 :s 3 :a 1) defender) 0))
+    (test/is (= (to-kill (model/create :ws 4 :s 3 :a 2) defender) 1/9))
+    (test/is (= (to-kill (model/create :ws 1 :s 3 :a 2) defender) 1/36))
+    (test/is (= (to-kill (model/create :ws 3 :s 4 :a 2) defender) 1/9))
+    (test/is (= (to-kill (model/create :ws 3 :s 5 :a 2) defender) 25/144)))
+  (let [defender (model/create :ws 3 :t 3 :w 1)]
+    (test/is (= (to-kill (model/create :ws 3 :s 3 :a 2) defender) 7/16))
+    (test/is (= (to-kill (model/create :ws 4 :s 3 :a 2) defender) 5/9))
+    (test/is (= (to-kill (model/create :ws 1 :s 3 :a 2) defender) 11/36))
+    (test/is (= (to-kill (model/create :ws 3 :s 4 :a 2) defender) 5/9))
+    (test/is (= (to-kill (model/create :ws 3 :s 5 :a 2) defender) 95/144))))
+
+(test/with-test
+  (defn to-score-exact-wounds
+    ([attacker defender wounds]
+       ())))
+
+
